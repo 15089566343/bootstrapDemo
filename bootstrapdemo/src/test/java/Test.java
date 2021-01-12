0639320c22f7da49d99c516263459c7ec605f246
huangshengwen <15089566343@163.com>
2021-01-12 15:37:19 +0800

    分支提交

---
+++ bootstrapdemo/src/test/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author huangshengwen
+ * @Date 2021/1/12 15:30
+ * @Version 1.0
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
